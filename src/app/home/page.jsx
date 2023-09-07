c76ceeecf1dcfe1a2ccc5dffecee0caf78b9b211
Haider Ali <hassanhussaian630@gmail.com>
2023-09-07 16:05:40 -0700

    commit

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { FaAngleDown } from "react-icons/fa";
+"use client";
+import React, { useState } from "react";
+import { FaAngleDown, FaPoll } from "react-icons/fa";
+import { AiOutlinePicture } from "react-icons/ai";
+import { GrLocation } from "react-icons/gr";
 
-const page = () => {
+const Page = () => {
+  const [tweet, setTweet] = useState("");
+
+  const handleTweetChange = (e) => {
+    const inputText = e.target.value;
+    if (inputText.length >= 280) {
+      setTweet(inputText);
+      
+    }
+  };
+
+  const handleTweetSubmit = () => {
+    console.log();
+  };
+
   return (
     <div className="w-[50%]">
       <div className="p-3">
         <h2 className="text-[18px] font-extralight">Home</h2>
       </div>
       {/* Tweet Box */}
-      <div className="w-full h-[14rem] border-t-2 border-b-2">
+      <div className="w-full h-[15rem] border-t-2 border-b-2">
         <div className="flex p-3">
           {/* profile logo */}
           <div className="w-[10%]">
@@ -19,23 +36,53 @@ const page = () => {
           {/* Tweet Box */}
           <div className="w-[90%]">
             {/* Privacy for tweet */}
-            <div className="p-1 w-fit text-blue-600 flex gap-x-1 ">
+            <div className="p-1 w-fit text-blue-600  gap-x-1 ">
               <select
                 name=""
                 id=""
                 className="border-2 rounded-lg outline-none"
               >
                 <option value="Everyone">
-                  Everyone <FaAngleDown />
+                  Everyone <FaAngleDown key="angle-down" />
                 </option>
                 <option value="Circle">Circle</option>
               </select>
             </div>
+            {/* Input for tweet box */}
+            <div>
+              <textarea
+                placeholder="What's happening?"
+                value={tweet}
+                onChange={handleTweetChange}
+                rows={5}
+                className="w-full p-2 outline-none resize-none border-b"
+              />
+            </div>
+            {/* Input for tweet box end */}
+            {/* tweet button */}
+            <div className="flex justify-between items-center">
+              <div className="flex gap-x-[3rem] text-[18px]">
+                <AiOutlinePicture key="picture" />
+                <FaPoll key="poll" />
+                <GrLocation key="location" />
+              </div>
+              
+              <div className="mr-9">
+                <button
+                  className="border px-4 py-2 bg-blue-700 rounded-[900px] text-white"
+                  onClick={handleTweetSubmit}
+                >
+                  Post
+                </button>
+              </div>
+            </div>
+            {/* Tweet button end */}
           </div>
         </div>
       </div>
+      {/* Tweet box end */}
     </div>
   );
 };
 
-export default page;
+export default Page;
